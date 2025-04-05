b3846de2edd19c098de9b1715078b9304ff9a73c
Ernesto Rampanelli Neto <ghostnetto@gmail.com>
2025-04-05 19:03:51 -0300

    funcional, refazer logo, e arrumar responsividade

--- script.js
+++ script.js
@@ -1,24 +1,28 @@
-let nome_filme = document.getElementById('nome_filme_id')
-let roteiro = document.getElementById('roteiro_id')
-let montagem = document.getElementById('montagem_id')
-let atuacao = document.getElementById('atuacao_id')
-let figurino = document.getElementById('figurino_id')
-let direcao = document.getElementById('direcao_id')
-let direcao_arte = document.getElementById('direcao_arte_id')
-let fotografia = document.getElementById('fotografia_id')
-let trama = document.getElementById('trama_id')
-let cgi = document.getElementById('cgi_id')
-
 
 
 function calcular_media() {
-    let rot = roteiro.value; // Captura o valor do <select>
-
-    if (rot === "") { // Verifica se o valor está vazio
-        window.alert("Por favor, selecione uma nota válida!");
-        return; // Sai da função para evitar processar valores inválidos
-    }
-
-    rot = Number(rot); // Converte para número depois de validar
-    window.alert(rot); // Mostra o valor selecionado
+    let selects = document.querySelectorAll('#selects_container2 select'); // Captura todos os selects
+    let soma = 0; // Inicializa a soma
+    selects.forEach(select => { // Itera sobre cada select
+        let valor = Number(select.value); // Converte o valor para número
+        if (!isNaN(valor)) { // Verifica se o valor é um número válido
+            soma += valor; // Adiciona o valor à soma total
+        }
+    });
+        let n_valores = 0; // Inicializa o contador
+        selects.forEach(select => {
+            if (select.value !== "") { // Verifica se o valor não está vazio
+                n_valores++; // Incrementa o contador
+            }
+        });
+        
+        let media = soma / n_valores
+        let nome_filme = document.getElementById('nome_filme_id')
+        let filme = nome_filme.value
+        if(n_valores > 0){
+            let pontos_display = document.getElementById('display_pontuacao')
+            pontos_display.innerHTML = `<p><span id="span1">Filme:${filme}<br></span>Nota: ${media.toFixed(1)}</p>`
+        }else{
+            window.alert(`Coloque algum valor nas caixas para gerar a nota.`);
+        }
 }
